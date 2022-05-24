05c7f1b03ff7880567256637b12993c9288d22ab
natsux30 <105287722+natsux30@users.noreply.github.com>
2022-05-24 09:13:27 -0300

    Add files via upload

--- java/js/java.js
+++ java/js/java.js
@@ -1,56 +1,77 @@
+//Variável onde pego os elementos do Li e Div input
 var btn = document.querySelectorAll(".key li"),
-input = document.querySelector(".input valor"),
-operador = document.querySelector(".operador");
+    input = document.querySelector(".input-valor"),
+    operador = document.querySelectorAll(".operador");
+
+
 for(var i = 0; i<btn.length; i++){
     document.onkeypress = function(event){
         var key = event.charCode;
-        for(var e = 0; e <= 10; e++){
-            if(key === (48+e)){
-                input.innerHTML += e;
+        //Mudança para o navegador
+        // firefox interpretar a calculadora
+    for(var e = 0; e <= 10; e++){
+        if(key === (48+e)){
+            input.innerHTML += e;
             }
         }
-        switch(key){
-                case 1:
-                input.innerHTML += "*";
+        switch (key){
+            case 42:
+            input.innerHTML += "*";
+            break;
+            case 43:
+            input.innerHTML += "+";
+            break;
+            case 45:
+            input.innerHTML += "-";
+            break;
+            case 46:
+            input.innerHTML += ".";
+            break;
+            case 47:
+            input.innerHTML += "/"
+            break;
+        case 13:
+        case 61:
+    var equacao = input.innerHTML;
+    if(equacao){
+        try{
+            input.innerHTML = eval(equacao);
+             }catch (e) {
+                alert ('Erro na expressão')
+             }
+        }
+        break;
+    case 67:
+    case 99:
+        input.innerHTML = "";
+        break;
+        default:
+        break;
+         }            
+    };
+    btn[i].addEventListener('click', function(){
+        var btnVal  = this.innerHTML,
+        inputVal = input.innerHTML;
+        //console.log(btnVal);
+        switch(btnval){
+            case "c":
+                input.innerHTML=""
                 break;
-                case 2:
-                input.innerHTML += "+"
-                break;
-                case 3:
-                input.innerHTML += "-"
-                break;
-                case 4:
-                input.innerHTML += "."
-                break;
-                case 5:
-                input.innerHTML += "/"
-                break;
-                case 6:
-                case 7:
-                    input.innerHTML = "";
-                    
-                    var equacao = input.innerHTML;
-                        try{
-                            input.innerHTML = eval(equacao);
-                        } catch (e){
-                            alert ('erro na expressão, idiota')
-                        }
-        
-                    
-                    
-                    break;
-                        case 89:
-                        case 9:
-                        input.innerHTML = "";
-                        break;
-                    default:
-                    break;
+                case"=":
+                var equacao = inputVal;
+                if(equacao){
+                    try{
+                        input.innerHTML = eval(equacao)
+                    } catch (e){
+                        alert("erro de expressão")
                     }
                 }
-        
+                break;
+                default:
+                    input.innerHTML += btnval
+                    break;
+
+        }
+    })
 
     }
-btn[i].addEventListener('click', function(){
-    var btnval = this.innerHTML,
-    inputval = input.innerHTML;
-})
